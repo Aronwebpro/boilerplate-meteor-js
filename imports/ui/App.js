9cf9af1774657b82257c5e8f4d44ee3c0ef87bd0
Aronwebpro <aronwebpro@gmail.com>
2018-10-31 11:04:42 -0500

    User management functionality

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,21 +2,29 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
+//Components
+import AuthenticatedRoute from './AuthenticatedRoute';
+
+//Pages
+import Login from './pages/Auth/Login';
+import ManagePassword from './pages/Auth/ManagePassword';
+
+//Handlers
+import { logOut } from '../api/logout';
 
 class App extends React.Component {
     state = {
         isMobile: window.innerWidth < 600,
     };
+
     render() {
         return (
             <Router>
                 <Switch>
-                    <Route path='/logout' render={() => { Meteor.logout(); return <Redirect to='/' push />}} />
-                    <Route path='/' render={() => (
-                        <div>
-                            Hello World!
-                        </div>
-                    )} />
+                    <Route path='/logout' render={logOut}/>
+                    <Route path='/enroll-account/:token' render={({ match }) => <ManagePassword token={match && match.params && match.params.token} firstPassword={true}/>}/>
+                    <AuthenticatedRoute path='/dashboard' render={() => <div>Hello Dashboard!</div>}/>
+                    <Route path='/' component={Login}/>
                 </Switch>
             </Router>
         );
